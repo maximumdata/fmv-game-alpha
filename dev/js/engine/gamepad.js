6922e52cb213e11dfd2fa6b2d110447d3955f288
Mike Dettmer <mike@mikedettmer.com>
2015-08-14 16:18:13 +0000

    added gamepad.js file

---
+++ dev/js/engine/gamepad.js
@@ -0,0 +1,10 @@
+addEvent(window, "gamepadconnected", function( e ) {
+  console.log("Gamepad connected at index %d: %s. %d buttons, %d axes.",
+    e.gamepad.index, e.gamepad.id,
+    e.gamepad.buttons.length, e.gamepad.axes.length);
+});
+
+addEvent(window, "gamepaddisconnected", function( e ) {
+  console.log("Gamepad disconnected from index %d: %s",
+    e.gamepad.index, e.gamepad.id);
+});
